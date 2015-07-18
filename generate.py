536e2b13a4c918392fdd041853a170db65ab0fa9
Eric Lemieux <ericlemieux2@gmail.com>
2015-07-18 15:09:54 -0400

    better gradiant backgrouds

--- generate.py
+++ generate.py
@@ -1,5 +1,10 @@
 #!/bin/python3.4
 
+import random
+
+def lerp(x1, x2, t):
+	return ((1-t)*x1 + t*x2)
+
 #This script will generate a wallpaper file so that it can be set later
 
 #Set some variable we are going to need
@@ -13,11 +18,24 @@ file.write("P3 ")
 s = str(str(WIDTH) + " " + str(HEIGHT) + " 255 ")
 file.write(s)
 
+START = [random.randint(100,200), random.randint(100,200), random.randint(100,200)]
+TOP_RIGHT = [0,0,0]
+
+BOT_LEFT = [random.randint(1,255), random.randint(1,255), random.randint(1,255)]
+
+while START[0] >= TOP_RIGHT[0]:
+	TOP_RIGHT[0] = random.randint(1,255)
+while START[1] >= TOP_RIGHT[1]:
+	TOP_RIGHT[1] = random.randint(1,255)
+while START[2] >= TOP_RIGHT[2]:
+	TOP_RIGHT[2] = random.randint(1,255)
+
 for y in range(HEIGHT):
 	for x in range(WIDTH):
-		file.write(str(int(x / WIDTH * 255)))
+		file.write(str(int((lerp(START[0], TOP_RIGHT[0], x/WIDTH)+lerp(START[0], BOT_LEFT[0],y/HEIGHT))/2)))
 		file.write(" ")
-		file.write(str(int(y /HEIGHT * 255)))
-		file.write(" 0 \n")		
-
+		file.write(str(int((lerp(START[1], TOP_RIGHT[1], x/WIDTH)+lerp(START[1], BOT_LEFT[1],y/HEIGHT))/2)))
+		file.write(" ")
+		file.write(str(int((lerp(START[2], TOP_RIGHT[2], x/WIDTH)+lerp(START[1], BOT_LEFT[2],y/HEIGHT))/2)))
+		file.write(" \n")
 file.close()
--- wallpaper.sh
+++ wallpaper.sh
@@ -3,4 +3,4 @@
 #This script will run the python file that generates a wallpaper and then sets the wallpaper using feh
 touch /tmp/wallpaper.ppm
 python generate.py
-#feh --bg-scale /tmp/wallpaper.ppm
+feh --bg-scale /tmp/wallpaper.ppm
